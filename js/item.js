942518bbb13b34582a169ec5fe594d6b33c3240f
Eduardo Alvarez <el_alienx@hotmail.com>
2021-03-24 20:10:01 +0100

    renamaed name for title in item.js

--- js/item.js
+++ js/item.js
@@ -1,11 +1,11 @@
-function createTaskItem(name) {
+function createTaskItem(title) {
   const item = document.createElement("li");
   const input = document.createElement("input");
   const span = document.createElement("span");
 
   input.setAttribute("type", "checkbox");
   input.addEventListener("change", removeTaskEvent);
-  span.innerText = name;
+  span.innerText = title;
 
   item.appendChild(input);
   item.appendChild(span);
